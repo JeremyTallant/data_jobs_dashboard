7c2f9de7b88905e2084be27fb9f225767f5df192
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-02-22 17:35:49 -0600

    debugging

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -1,27 +1,14 @@
-d3.json("api/v1.0/states", function (error, data) {
+d3.json("http://127.0.0.1:5050/api/v1.0/states", function (error, data) {
     if (error) {
         console.error(error);
     } else {
         console.log(data); // log the data to the console
         // Extract the data we want to plot (state names and median household incomes)
-        var plotData = data.map(function (d) {
-            return {
-                state: d.Name,
-                median_income: +d["Median household income"]
-            };
-        });
-
-        // Sort the data in descending order by median household income
-        plotData.sort(function (a, b) {
-            return b.median_income - a.median_income;
-        });
-
-        // Extract the state names and median household incomes as separate arrays
-        var stateNames = plotData.map(function (d) {
-            return d.state;
-        });
-        var medianIncomes = plotData.map(function (d) {
-            return d.median_income;
+        var stateNames = data.map(function (d) { return d['Name']; });
+        var medianIncomes = data.map(function (d) {
+            var income = d['Median household income'];
+            // Strip the dollar sign and space from the income string, and parse it as a number
+            return parseFloat(income.replace(/\$| /g, ''));
         });
 
         // Create the bar chart using Plotly
