199e4014bf397b1e0c45869f9bd2e7cd2c1e654d
tallantj05 <jeremytallant@Jeremys-MBP.lan>
2023-02-22 22:14:45 -0600

    made changes to logic1.js

--- static/js/logic1.js
+++ static/js/logic1.js
@@ -1,30 +1,31 @@
-d3.json("http://127.0.0.1:5050/api/v1.0/states", function (error, data) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(data); // log the data to the console
+// Make a request to the API
+fetch('http://127.0.0.1:5050/api/v1.0/states')
+    .then(response => response.json())
+    .then(data => {
+
         // Extract the data we want to plot (state names and median household incomes)
-        var stateNames = data.map(function (d) { return d['Name']; });
-        var medianIncomes = data.map(function (d) {
-            var income = d['Median household income'];
-            // Strip the dollar sign and space from the income string, and parse it as a number
-            return parseFloat(income.replace(/\$| /g, ''));
-        });
+        var stateIncomes = data.map(d => ({
+            state: d.Name,
+            medianIncome: (d['Median household income'])
+        }));
+
+
 
         // Create the bar chart using Plotly
         var data = [{
-            x: stateNames,
-            y: medianIncomes,
+            x: stateIncomes.map(d => d.state),
+            y: stateIncomes.map(d => d.medianIncome),
             type: 'bar'
         }];
         var layout = {
             xaxis: {
-                title: 'State'
+                title: 'State',
+                tickangle: 45
             },
             yaxis: {
                 title: 'Median Household Income ($)'
             }
         };
         Plotly.newPlot('barchart', data, layout);
-    }
-});
+    })
+    .catch(error => console.error(error));
